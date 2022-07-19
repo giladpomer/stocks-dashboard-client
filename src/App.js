a6e44b7b2dde89d6e4e69c919934133235d25768
Gilad Pomer <giladpomer@gmail.com>
2022-07-19 19:24:51 +0200

    stocks table with mock data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Table from 'react-bootstrap/Table';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>
+                Stocks Dashboard
+            </h1>
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Value</th>
+                        <th>Amount</th>
+                        <th>Sum</th>
+                        <th>Daily Change</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Tesla</td>
+                        <td>100</td>
+                        <td>2</td>
+                        <td>200</td>
+                        <td>+1.00%</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
+    );
 }
 
 export default App;
