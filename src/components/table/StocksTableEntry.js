4644eb507159b615113fccacca82ec6ed5f2665c
Gilad Pomer <giladpomer@gmail.com>
2022-07-22 22:08:48 +0200

    color on stock value change

--- src/components/table/StocksTableEntry.js
+++ src/components/table/StocksTableEntry.js
@@ -1,4 +1,29 @@
+import React from 'react';
+
 export default function StocksTableEntry(props) {
+    const [_stockValue, setStockValue] = React.useState(null);
+    const [_stockValueChange, setStockValueChange] = React.useState(0);
+
+    React.useEffect(() => {
+        setStockValue(prevValue => {
+            var newValue = props.data.liveData?.value;
+
+            setStockValueChange(newValue - prevValue);
+
+            return newValue;
+        });
+
+        const timeoutId = setTimeout(clearStockValueChange, 3000);
+
+        return function cleanup() {
+            clearTimeout(timeoutId);
+        };
+    }, [props.data.liveData?.value]);
+
+    function clearStockValueChange() {
+        setStockValueChange(0);
+    }
+
     function formatDailyChange(dailyChange) {
         if (!dailyChange) {
             return '';
@@ -11,13 +36,39 @@ export default function StocksTableEntry(props) {
         return dailyChange;
     }
 
+    function getChangeClassName(dailyChange) {
+        if (dailyChange == null) {
+            return '';
+        }
+
+        if (dailyChange > 0) {
+            return 'up';
+        }
+
+        if (dailyChange < 0) {
+            return 'down';
+        }
+
+        return '';
+    };
+
     return (
-        <tr>
-            <td>{props.data.name}</td>
-            <td>{props.data.liveData?.value ?? ''}</td>
-            <td>{props.data.amount}</td>
-            <td>{props.data.liveData?.sum?.toLocaleString('en-US', { minimumFractionDigits: 2 }) ?? ''}</td>
-            <td>{formatDailyChange(props.data.liveData?.dailyChange) + '%'}</td>
+        <tr className='stock'>
+            <td>
+                {props.data.name}
+            </td>
+            <td className={getChangeClassName(_stockValueChange)}>
+                {props.data.liveData?.value ?? ''}
+            </td>
+            <td>
+                {props.data.amount?.toLocaleString('en-US')}
+            </td>
+            <td className={getChangeClassName(_stockValueChange)}>
+                {props.data.liveData?.sum?.toLocaleString('en-US', { minimumFractionDigits: 2 }) ?? ''}
+            </td>
+            <td className={getChangeClassName(props.data.liveData?.dailyChange)}>
+                {formatDailyChange(props.data.liveData?.dailyChange)?.toLocaleString('en-US', { minimumFractionDigits: 2 }) + '%'}
+            </td>
         </tr>
     );
 }
